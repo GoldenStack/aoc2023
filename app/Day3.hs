b6d9a84d9ba798e48853ec624fdd682fab0e5c13
GoldenStack <goldenfire64yt@gmail.com>
2023-12-03 22:47:03 -0600

    Support both parts of Day 3

--- app/Day3.hs
+++ app/Day3.hs
@@ -35,41 +35,40 @@ neighbors = [(x, y) | x <- [-1, 0, 1], y <- [-1, 0, 1]]
 add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 -- Figure the valid parts that are neighbors
-parts :: Int -> Int -> [[Char]] -> [(Int, Int)]
-parts x y board = filter (isGear . (\(x2, y2) -> get x2 y2 board)) $ map (add (x, y)) neighbors
+parts :: (Char -> Bool) -> Int -> Int -> [[Char]] -> [(Int, Int)]
+parts predicate x y board = filter (predicate . (\(x2, y2) -> get x2 y2 board)) $ map (add (x, y)) neighbors
 
 -- Figure out if a tile has any parts that are neighbors
--- isPart = (\i -> length i > 0) . parts
-isPart x y board = not $ null $ parts x y board
+isPart predicate x y board = not $ null $ parts predicate x y board
 
 -- Parse an integer value at the given position.
-parseValue :: Int -> Int -> Board -> ([Char], [(Int, Int)])
-parseValue x y board = if nextIsNum then (get x y board : fst next, part ++ snd next) else ([get x y board], nub part)
+parseValue :: (Char -> Bool) -> Int -> Int -> Board -> ([Char], [(Int, Int)])
+parseValue predicate x y board = if nextIsNum then (get x y board : fst next, part ++ snd next) else ([get x y board], nub part)
   where
-    next = parseValue (x + 1) y board
-    part = parts x y board
+    next = parseValue predicate (x + 1) y board
+    part = parts predicate x y board
     nextIsNum = isDigit (get (x + 1) y board)
 
 -- Parse a line for all of its numbers
-parseLine :: Int -> Int -> Board -> [([Char], [(Int, Int)])]
-parseLine x y board = if exit then [] else value
+parseLine :: (Char -> Bool) -> Int -> Int -> Board -> [([Char], [(Int, Int)])]
+parseLine predicate x y board = if x >= height board then [] else value
   where
-    exit = x >= height board
     isNumber = isDigit $ get x y board
-    numberParse = parseValue x y board
-    next = x + if isNumber then length (fst numberParse) else 1
-    value = if isNumber then numberParse : parseLine next y board else parseLine next y board
+    numberParse = parseValue predicate x y board
+    xOffset = if isNumber then length (fst numberParse) else 1
+    next = parseLine predicate (x + xOffset) y board
+    value = if isNumber then numberParse : next else next
 
 -- Parse the entire board for all of its numbers.
-parseBoard board = [v | y <- [0 .. height board], v <- parseLine 0 y board]
+parseBoard predicate board = [v | y <- [0 .. height board], v <- parseLine predicate 0 y board]
 
 -- Sum the valid numbers in the board
-day3p1 = sum . map (read . fst) . filter (not . null . snd) . parseBoard
+day3p1 = sum . map (read . fst) . filter (not . null . snd) . parseBoard isSymbol
 
 -- Sum the valid gears in the board.
 day3p2 board = total
   where
-    nums = parseBoard board
+    nums = parseBoard isGear board
     expanded = map (\i -> map (fst i,) (snd i)) nums
     flattened = [a | b <- expanded, a <- b]
     keys = nub $ map snd flattened
