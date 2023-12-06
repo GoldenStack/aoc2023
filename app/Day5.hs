d35fa77c4c97e0615e143bcbf57d0101a7b0fa44
GoldenStack <goldenfire64yt@gmail.com>
2023-12-05 19:25:06 -0600

    Add Day 5 template

---
+++ app/Day5.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day5 where
+
+import Data.String.Conversions (cs)
+import Data.Text (Text, length, replace, splitOn)
+
+day5p1 = undefined
+
+day5p2 = undefined
--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,14 @@ import Day1 (day1p1, day1p2)
 import Day2 (day2p1, day2p2)
 import Day3 (day3p1, day3p2)
 import Day4 (day4p1, day4p2)
+import Day5 (day5p1, day5p2)
 
 main :: IO ()
 main = do
-  handle <- readFile "./input/day4.txt"
+  handle <- readFile "./input/day5.txt"
 
   let lines = splitOn "\n" handle
   let textLines = map cs lines
 
-  print $ day4p2 textLines
+  print $ day5p1 textLines
+  print $ day5p2 textLines
